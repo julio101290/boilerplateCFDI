b0a6fd799e204b0232c4fc79e38caca26d49df17
juliocesar101290 <juliocesarleyvarodriguez@hotmail.com>
2025-01-17 09:33:59 -0700

    first commit

---
+++ src/Language/es/xml.php
@@ -0,0 +1,31 @@
+ <?php
+$xml["logDescription"] = "El registro en xml fue guardado con los siguientes datos:";
+$xml["logUpdate"] = "El registro en xml fue actualizado con los siguientes datos:";
+$xml["logDeleted"] = "El registro en xml fue eliminado con los siguientes datos:";
+$xml["msg_delete"] = "El Registro en xml fue eliminado correctamente:";
+$xml["add"] = "Agregar Xml";
+$xml["edit"] = "Editar xml";
+$xml["createEdit"] = "Crear / Editar";
+$xml["title"] = "Admon. xml";
+$xml["subtitle"] = "Lista xml";
+$xml["fields"]["uuidTimbre"] = "UuidTimbre";
+$xml["fields"]["archivoXML"] = "ArchivoXML";
+$xml["fields"]["serie"] = "Serie";
+$xml["fields"]["folio"] = "Folio";
+$xml["fields"]["tipoComprobante"] = "TipoComprobante";
+$xml["fields"]["fecha"] = "Fecha";
+$xml["fields"]["fechaTimbrado"] = "FechaTimbrado";
+$xml["fields"]["total"] = "Total";
+$xml["fields"]["created_at"] = "Created_at";
+$xml["fields"]["deleted_at"] = "Deleted_at";
+$xml["fields"]["updated_at"] = "Updated_at";
+$xml["fields"]["uuidPaquete"] = "UuidPaquete";
+
+ $xml["fields"]["actions"] = "Acciones";       
+$xml["msg"]["msg_insert"] = "Registro agregado correctamente.";
+$xml["msg"]["msg_update"] = "Registro modificado correctamente.";
+$xml["msg"]["msg_delete"] = "Registro eliminado correctamente.";
+$xml["msg"]["msg_get"] = "Registro obtenido correctamente.";
+$xml["msg"]["msg_get_fail"] = "Registro no encontrado o eliminado.";
+return $xml;
+        
